c2fed5d0b40ee3dc6e0dbd27e1640b575fa53db4
Yuya Kakui <mail@kakuiyuya.com>
2014-05-11 15:05:22 +0900

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+KikuponTop::Application.config.secret_key_base = '7e3c2e86ca8bb85453ab15ec5f8aacaacaaf89efb0e1584cbd472355fd53f76dbc1cfe437e6d0caf200575f627d9f543c9423e069b1c95815051bd0a4f44f65a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+KikuponTop::Application.config.session_store :cookie_store, key: '_kikupon_top_session'
